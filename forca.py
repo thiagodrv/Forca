7f9fdb4e2f265f92a22f65be1c0ca67680e6c521
Thiago Varela <thiagodrv@gmail.com>
2012-07-03 09:08:28 -0300

    pra ver se funciona

--- forca.py
+++ forca.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python2
 
-import sys
+import sys, string
 
-file = 'wordlist-preao-20120701.txt'
+file = 'wordlist.txt'
 fd = open(file, 'r')
-wordlist = fd.read()
-#open(file, 'w')
-#wordlist = fd.read()
 
 if __name__=='__main__':
+    #wordlist = fd.read()
     palavra = sys.argv[1]
     comprimento = len(palavra)
     print palavra
     print comprimento
-    print len(wordlist)
     #apenas retirar da wordlist palavras com o mesmo numero de letras
-    for line in wordlist:
-        print 'entrou'
-        print 'linha: '+line
-        if len(line) == comprimento:
-            print line
+    lines = fd.readlines()
+    for line in lines:
+        line = line.replace('\n', '')
+        compri_linha = len(line)
+        hyphen = line.count('-')
+        if hyphen != 0:
+            compri_linha = compri_linha - hyphen
+        if compri_linha == comprimento:
+            #print 'entrou1'
+            for i in range(comprimento):
+                #print 'entrou2'
+                if palavra[i] == '_':
+                    #print 'entrou3'
+                    pass
+                if line[i] == palavra[i]:
+                    #print 'entrou4'
+                    print line
+            
+            #for i in palavra:
+            #    if i != '_':
+            #print line
